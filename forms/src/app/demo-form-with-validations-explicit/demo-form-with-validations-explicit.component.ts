a6a61355d600920d44d57df6c7a097539f9b2e9e
netfisher777 <netfisher777@gmail.com>
2018-03-27 23:27:41 +0300

    create form with validations explicit

---
+++ forms/src/app/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, AbstractControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-demo-form-with-validations-explicit',
+  templateUrl: './demo-form-with-validations-explicit.component.html',
+  styleUrls: ['./demo-form-with-validations-explicit.component.css']
+})
+export class DemoFormWithValidationsExplicitComponent implements OnInit {
+
+  myForm: FormGroup;
+  sku: AbstractControl;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['', Validators.required] // it's the same as FormControl(value, validator)
+    });
+    this.sku = this.myForm.controls['sku'];
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('submitted value 3:', value, this.sku);
+  }
+
+}

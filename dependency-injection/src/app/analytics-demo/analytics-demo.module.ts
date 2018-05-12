a1567ecb6cddd248cc40769e5fe8abb7ed66f10d
netfisher777 <netfisher777@gmail.com>
2018-05-12 19:34:01 +0300

    update to angular 6

---
+++ dependency-injection/src/app/analytics-demo/analytics-demo.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Http, HttpModule} from '@angular/http';
+import {AnalyticsService} from '../services/analytics.service';
+import {AnalyticsImplementation, Metric} from './analytics-demo.interface';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpModule
+  ],
+  providers: [
+    {
+      provide: 'API_URL',
+      useValue: 'http://devserver.com'
+    },
+    {
+      provide: AnalyticsService,
+      deps: [Http, 'API_URL'],
+      useFactory(http: Http, apiUrl: string) {
+        const loggingImplementation: AnalyticsImplementation = {
+          recordEvent: (metric: Metric): void => {
+            console.log('The metric is:', metric);
+            console.log('sending to:', apiUrl);
+          }
+        };
+        return new AnalyticsService(loggingImplementation);
+      }
+    }
+  ],
+  declarations: []
+})
+export class AnalyticsDemoModule {
+
+}
